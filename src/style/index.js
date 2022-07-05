897b6863b8b953ef44fd309c029f4efcc4e07ee6
Bruno Tiberio <quentemente@gmail.com>
2022-07-05 10:21:00 -0300

    feat: estrutura do projeto + globalstyled

---
+++ src/style/index.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const StyledGlobal = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+        
+        button{
+            cursor: pointer;
+
+            &&:hover{
+                filter: brightness(0.8)
+            }
+        }
+    }
+`
