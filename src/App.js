c6576c11939d27eb99fcabf29ea64283ce652fc2
Felipe Campregher <felipe.campregher@gmail.com>
2022-07-07 18:28:14 -0300

    criação da pasta contexts, criado userContext do login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { Header } from "./components/header";
 import { StyledMain } from "./components/main/style";
-import CompanyRegistration from "./components/company-registration";
-import Login from "./components/login";
-import PersonRegistration from "./components/person-registration";
-import { Header } from "./components/header";
 import { Main } from "./components/main";
 import { StyledGlobal } from "./style";
+import Login from "./components/login";
 
 function App() {
   return (
     <>
-    <StyledGlobal/>
-        <Header />
-      <Main>
-      </Main>
+      <StyledGlobal />
+      <Header />
+      <Main></Main>
+      <Login />
     </>
   );
 }
