3ae14698cf8853907da6260bf510a8731c5c8d16
lVladimirl <infinitdungeun@gmail.com>
2022-07-07 15:33:32 -0300

    feat/main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Header } from "./components/header";
+import { StyledMain } from "./components/main/style";
 import { StyledGlobal } from "./style";
 
 function App() {
@@ -6,6 +7,9 @@ function App() {
     <>
       <StyledGlobal />
       <Header />
+      <StyledMain>
+        
+      </StyledMain>
     </>
   );
 }
