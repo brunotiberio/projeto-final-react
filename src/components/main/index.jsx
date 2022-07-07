1e03d0fec16646d389a370b8db3a644289077c40
Cayo Nakasato <cayonakasato112@gmail.com>
2022-07-07 17:51:13 -0400

    feat: create comment card, with some customization in it

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,11 +1,10 @@
-import { PostComments } from "../postComments";
+import { CardComments } from "../cardComments";
 import { StyledMain } from "./style";
-import {NewsBody} from "../newsBody/"
+
 export const Main = () => {
   return (
     <StyledMain>
-      <NewsBody title="Lorem" url="https://www.infoescola.com/wp-content/uploads/2018/03/sagui-de-tufos-brancos_790108426.jpg" imageDescription="Macaco" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente exercitationem illum, eos ipsa ea optio corporis sequi cupiditate. Repellendus sit inventore eius velit modi, similique facilis distinctio asperiores iusto? Necessitatibus." />
-      <PostComments/>
+      <CardComments/>
     </StyledMain>
   );
 };
