8f0243638a7567a5f8ba7cdf4177a9c1120639ad
Cayo Nakasato <cayonakasato112@gmail.com>
2022-07-07 16:37:17 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import { Header } from "./components/header";
-import { StyledMain } from "./components/main/style";
-import CompanyRegistration from "./components/company-registration";
-import Login from "./components/login";
-import PersonRegistration from "./components/person-registration";
-import { Header } from "./components/header";
 import { Main } from "./components/main";
 import { StyledGlobal } from "./style";
 
