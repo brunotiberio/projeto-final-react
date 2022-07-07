1285192c4da9c70b6a9db45eeb986d851d396dd6
lVladimirl <infinitdungeun@gmail.com>
2022-07-07 18:27:04 -0300

    feat/newsRating

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import { Header } from "./components/header";
-import { StyledMain } from "./components/main/style";
-import CompanyRegistration from "./components/company-registration";
-import Login from "./components/login";
-import PersonRegistration from "./components/person-registration";
-import { Header } from "./components/header";
 import { Main } from "./components/main";
 import { StyledGlobal } from "./style";
 
 function App() {
   return (
     <>
-    <StyledGlobal/>
-        <Header />
-      <Main>
-      </Main>
+      <StyledGlobal />
+      <Header />
+      <Main />
     </>
   );
 }
--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,8 +1,9 @@
 import { StyledMain } from "./style";
+import {NewsBody} from "../newsBody/"
 export const Main = () => {
   return (
     <StyledMain>
-      
+      <NewsBody title="Lorem" url="https://www.infoescola.com/wp-content/uploads/2018/03/sagui-de-tufos-brancos_790108426.jpg" imageDescription="Macaco" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente exercitationem illum, eos ipsa ea optio corporis sequi cupiditate. Repellendus sit inventore eius velit modi, similique facilis distinctio asperiores iusto? Necessitatibus." />
     </StyledMain>
   );
 };
