686ee77392a840e7623b47b214bca1409eb43815
Cayo Nakasato <cayonakasato112@gmail.com>
2022-07-08 16:29:12 -0400

    feat: Incompleted page push to fix files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Header } from "./components/header";
 import { Main } from "./components/main";
+import { SingleArticlePage } from "./pages/singleArticlePage";
 import Paths from "./routes";
 import { StyledGlobal } from "./style";
 
@@ -10,6 +11,7 @@ function App() {
 
       <Header />
       <Main>
+        <SingleArticlePage/>
         <Paths />
       </Main>
     </>
