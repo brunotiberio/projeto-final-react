3c474b2d5c61db568f10dff74c226f682c2aa325
Bruno Tiberio <quentemente@gmail.com>
2022-07-10 15:25:16 -0300

    styled: editNews

--- src/components/editNews/index.jsx
+++ src/components/editNews/index.jsx
@@ -3,7 +3,7 @@ import { useForm, useInput } from "lx-react-form";
 
 import { Button, TextField } from '@mui/material'
 
-import { FormContainer } from './style'
+import { FormContainer, InputsContainer } from './style'
 
 export default function EditNews() {
 
@@ -36,50 +36,57 @@ export default function EditNews() {
   return (
     <>
     <FormContainer>
+        <h2>Editar notícia</h2>
         <form onSubmit={form.handleSubmit}>
-            <TextField
-                type="text"
-                error={title.error && true}
-                label="Título da notícia"
-                helperText={title.error}
-                {...title.inputProps}
-            />
+            <InputsContainer>
+                <TextField
+                    type="text"
+                    error={title.error && true}
+                    label="Título da notícia"
+                    helperText={title.error}
+                    {...title.inputProps}
+                    style={{width: '100%', marginTop: 20}}
+                />
 
-            <TextField
-                type="link"
-                error={urlToImage.error && true}
-                label="Imagem da notícia (url)"
-                helperText={urlToImage.error}
-                {...urlToImage.inputProps}
-            />
+                <TextField
+                    type="link"
+                    error={urlToImage.error && true}
+                    label="Imagem da notícia (url)"
+                    helperText={urlToImage.error}
+                    {...urlToImage.inputProps}
+                    style={{width: '100%', marginTop: 20}}
+                />
+            </InputsContainer>
             
 
             <TextField
                 type="text"
                 multiline
-                minRows={10}
+                minRows={5}
                 maxRows={10}                
                 error={description.error && true}
                 label="Descrição da notícia"
                 helperText={description.error}
                 {...description.inputProps}
+                style={{width: '95%', marginTop: 20}}
             />
            
 
             <TextField
                 type="text"
                 multiline
-                minRows={20}
+                minRows={10}
                 maxRows={20}
                 error={content.error && true}
                 label="Notícia geral"
                 helperText={content.error}
                 {...content.inputProps}
+                style={{width: '95%', marginTop: 20}}
             />
             
 
-            <Button variant="contained" type="submit"> Criar notícia </Button>
-            <Button variant="contained" type="submit"> Voltar </Button>
+            <Button variant="contained" type="submit" style={{width: '95%', marginTop: 20}}> Criar notícia </Button>
+            <Button variant="contained" type="submit" style={{width: '95%', marginTop: 20, marginBottom: 20}}> Voltar </Button>
         </form>
     </FormContainer>
     </>
